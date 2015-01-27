aead81a50a18a3bab957294393c2adb1dd28f53a
Ig M <igsmar@gmail.com>
2015-01-26 21:20:32 -0800

    Re project, it's all working from my end.

---
+++ app/src/main/java/com/schedufy/user/schedufy/EventsFragment.java
@@ -0,0 +1,13 @@
+package com.schedufy.user.schedufy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class EventsFragment extends SherlockFragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+}
---
+++ app/src/main/java/com/schedufy/user/schedufy/MainActivity.java
@@ -0,0 +1,40 @@
+package com.schedufy.user.schedufy;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+public class MainActivity extends SherlockFragmentActivity {
+    private ViewPager mViewPager;
+    private TabsAdapter mTabsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.pager);
+        setContentView(mViewPager);
+
+        final ActionBar bar = getSupportActionBar();
+        bar.setTitle("Schedufy");
+        bar.setBackgroundDrawable(getResources().getDrawable(R.color.schedufy_red));
+        bar.setStackedBackgroundDrawable(getResources().getDrawable(R.color.schedufy_red));
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        mTabsAdapter = new TabsAdapter(this, mViewPager);
+        mTabsAdapter.addTab(bar.newTab().setText("Home"), HomeFragment.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText("Calendar"), CalendarFragment.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText("Events"), EventsFragment.class, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}

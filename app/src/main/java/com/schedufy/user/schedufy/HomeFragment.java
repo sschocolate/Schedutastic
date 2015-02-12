43eb80f9f7ffc369826f651aed3875ff8ca4ca68
Ig M <igsmar@gmail.com>
2015-02-11 20:37:39 -0800

    albjadf

--- app/src/main/java/com/schedufy/user/schedufy/HomeFragment.java
+++ app/src/main/java/com/schedufy/user/schedufy/HomeFragment.java
@@ -1,12 +1,9 @@
 package com.schedufy.user.schedufy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 

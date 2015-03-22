a840e80ec636f91ab2c044408d1c527d5dc37bbc
Ig M <igsmar@gmail.com>
2015-03-22 13:46:32 -0700

    Added a couple comments to clarify what the how the database works and how the fragment is using the database.

--- app/src/main/java/com/schedufy/user/schedufy/EventDatabaseAdapter.java
+++ app/src/main/java/com/schedufy/user/schedufy/EventDatabaseAdapter.java
@@ -6,38 +6,66 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * Database adapter class: manages database.
+ */
 public class EventDatabaseAdapter extends SQLiteOpenHelper {
     public SQLiteDatabase db;
 
+    // Database details
     public static final String DATABASE_NAME = "events_database";
     public static final String TABLE_NAME = "events_table";
     public static final int DATABASE_VERSION = 1;
 
+    // Database columns
     public static final String COL_UID = "_id";
     public static final String COL_CATEGORY = "Category";
     public static final String COL_DATE = "Date";
     public static final String COL_TIME = "Time";
     public static final String COL_DESCRIPTION = "Description";
 
+    // SQL commands
     private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + COL_UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_CATEGORY + " VARCHAR(255), " + COL_DATE + " VARCHAR(255), " + COL_TIME + " VARCHAR(255), " + COL_DESCRIPTION + " VARCHAR(255));";
     private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
+    /**
+     * Constructor: calls base class SQLiteOpenHelper constructor
+     * @param context
+     */
     public EventDatabaseAdapter(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Creates the table using SQL command
+     * @param db
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL(CREATE_TABLE);
     }
 
+    /**
+     * When the database changes upgrade will change the version
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_TABLE);
         onCreate(db);
     }
 
+    /**
+     * Inserts an record (an event) into the database.
+     * @param category
+     * @param date
+     * @param time
+     * @param description
+     * @return
+     */
     public long insertEvent(String category, String date, String time, String description) {
         db = getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -51,6 +79,18 @@ public class EventDatabaseAdapter extends SQLiteOpenHelper {
         return id;
     }
 
+    /**
+     * Removes a record (an event) from the database.
+     * @param uid
+     */
+    public void removeEvent(String uid) {
+        db = getWritableDatabase();
+    }
+
+    /**
+     * Retrieves all the records in the database.
+     * @return Records stored in a cursor
+     */
     public Cursor getAllRows() {
         db = getWritableDatabase();
         String columns[] = {COL_UID, COL_CATEGORY, COL_DATE, COL_TIME, COL_DESCRIPTION};
--- app/src/main/java/com/schedufy/user/schedufy/HomeFragment.java
+++ app/src/main/java/com/schedufy/user/schedufy/HomeFragment.java
@@ -3,47 +3,118 @@ package com.schedufy.user.schedufy;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
+/**
+ * A ListView of database items.
+ * A button to switch activities where items can be added to the database.
+ */
 public class HomeFragment extends ListFragment {
+    // Constraints for the fling gesture
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 
-    private EventDatabaseAdapter dbAdapter;
+    private View view;
     private ListView list;
-    private SimpleCursorAdapter cursorAdapter;
-    private Cursor dbCursor;
 
+    private EventDatabaseAdapter mDbAdapter;
+    private Cursor mDbCursor;
+    private SimpleCursorAdapter mCursorAdapter;
+    private GestureDetector mGestureDetector;
+    View.OnTouchListener mGestureListener;
+
+    /**
+     * Shows ListView of database items.
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        dbAdapter = new EventDatabaseAdapter(getActivity());
-        dbAdapter.getWritableDatabase();
-        dbCursor = dbAdapter.getAllRows();
+        mDbAdapter = new EventDatabaseAdapter(getActivity());
+        mDbAdapter.getWritableDatabase();
+        mDbCursor = mDbAdapter.getAllRows();
 
         list = (ListView) view.findViewById(android.R.id.list);
 
+        mGestureDetector = new GestureDetector(getActivity(), new GestureDetection());
+        mGestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return mGestureDetector.onTouchEvent(event);
+            }
+        };
+
         setList(view);
+        list.setOnTouchListener(mGestureListener);
 
         return view;
     }
 
-    public void setList(View v)
-    {
-        cursorAdapter = new SimpleCursorAdapter(
-            getActivity(),
-            R.layout.list_item,
-            dbCursor,
-            new String[]{EventDatabaseAdapter.COL_UID, EventDatabaseAdapter.COL_CATEGORY, EventDatabaseAdapter.COL_DATE, EventDatabaseAdapter.COL_TIME, EventDatabaseAdapter.COL_DESCRIPTION},
-            new int[]{R.id.list_uid, R.id.list_category, R.id.list_date, R.id.list_time, R.id.list_description},
-            0
+    /**
+     * Dynamically update ListView of database items.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mDbCursor = mDbAdapter.getAllRows();
+        setList(view);
+    }
+
+    /**
+     * Used for the detection of swiping on ListView items.
+     */
+    class GestureDetection extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
+                    return false;
+                }
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(getActivity(), "LEFT", Toast.LENGTH_LONG).show();
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(getActivity(), "RIGHT", Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+    }
+
+    /**
+     * Sets the ListView adapter to the SimpleCursorAdapter which maps the database column into a ListView item layout.
+     * @param v
+     */
+    public void setList(View v) {
+        mCursorAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.list_item,
+                mDbCursor,
+                new String[]{EventDatabaseAdapter.COL_UID, EventDatabaseAdapter.COL_CATEGORY, EventDatabaseAdapter.COL_DATE, EventDatabaseAdapter.COL_TIME, EventDatabaseAdapter.COL_DESCRIPTION},
+                new int[]{R.id.list_uid, R.id.list_category, R.id.list_date, R.id.list_time, R.id.list_description},
+                0
         );
 
-        list.setAdapter(cursorAdapter);
+        list.setAdapter(mCursorAdapter);
     }
 }

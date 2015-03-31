b5988d9d05c714211bc2b46a5a4a239cf1b32045
Ig M <igsmar@gmail.com>
2015-03-31 11:56:46 -0700

    push me

--- app/src/main/java/com/schedufy/user/schedufy/CalendarFragment.java
+++ app/src/main/java/com/schedufy/user/schedufy/CalendarFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class CalendarFragment extends SherlockFragment {
 
+    private View view;
+    private CalendarView calendar;
+
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-        return inflater.inflate(R.layout.fragment_calendar,
-                container, false);
+        return view;
     }
 }
